952d1c398eecb0dbce2f38209c52d2a47df1adc7
visquis-miaffossa <visquis.miaffossa@kambriq.com>
2026-05-11 07:41:43 +0200

    Revert "fix(types): add type to land.media and reshape mock reservations to landreservation (#21)"
    This reverts commit b722f9239528224f6be4b63c4cd907d00b069a6d.

--- apps/web/src/types/lands.ts
+++ apps/web/src/types/lands.ts
@@ -59,7 +59,7 @@ export interface Land {
   isPublished: boolean;
   isVerified: boolean;
   status: LandStatus;
-  media: { id: string; url: string; downloadUrl: string; type: LandMediaType }[];
+  media: { id: string; url: string; downloadUrl: string }[];
   documents?: LandDocument[];
   createdAt: string;
   updatedAt: string;
